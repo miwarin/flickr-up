cb1c43c58038fe942d7677b8e7d265c0c9f67766
Susumu Miwa <miwarin@gmail.com>
2014-03-01 15:18:56 +0900

    flickr認証だけ

--- flickr-up/frmMain.Designer.cs
+++ flickr-up/frmMain.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAuth = new System.Windows.Forms.Button();
+            this.btnAuthComplete = new System.Windows.Forms.Button();
+            this.txtVerifier = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAuth
+            // 
+            this.btnAuth.Location = new System.Drawing.Point(47, 31);
+            this.btnAuth.Name = "btnAuth";
+            this.btnAuth.Size = new System.Drawing.Size(298, 53);
+            this.btnAuth.TabIndex = 0;
+            this.btnAuth.Text = "認証";
+            this.btnAuth.UseVisualStyleBackColor = true;
+            this.btnAuth.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAuthComplete
+            // 
+            this.btnAuthComplete.Location = new System.Drawing.Point(47, 115);
+            this.btnAuthComplete.Name = "btnAuthComplete";
+            this.btnAuthComplete.Size = new System.Drawing.Size(298, 53);
+            this.btnAuthComplete.TabIndex = 1;
+            this.btnAuthComplete.Text = "完了";
+            this.btnAuthComplete.UseVisualStyleBackColor = true;
+            this.btnAuthComplete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtVerifier
+            // 
+            this.txtVerifier.Location = new System.Drawing.Point(47, 90);
+            this.txtVerifier.Name = "txtVerifier";
+            this.txtVerifier.Size = new System.Drawing.Size(298, 19);
+            this.txtVerifier.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(47, 174);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(298, 196);
+            this.txtResult.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(435, 394);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtVerifier);
+            this.Controls.Add(this.btnAuthComplete);
+            this.Controls.Add(this.btnAuth);
+            this.Name = "frmMain";
+            this.Text = "Flickr認証";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAuth;
+        private System.Windows.Forms.Button btnAuthComplete;
+        private System.Windows.Forms.TextBox txtVerifier;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
 
--- flickr-up/frmMain.cs
+++ flickr-up/frmMain.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FlickrNet;
 
 namespace flickr_up
 {
     public partial class frmMain : Form
     {
+        private String APIKey = "apikey";
+        private String Secret = "secret";
+        private OAuthRequestToken RequestToken;
+
         public frmMain()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Flickr flickr = new Flickr(APIKey, Secret);
+
+            // what's "oob" ?
+            // I have tried "flickr-up", then error. hmm...
+            RequestToken = flickr.OAuthGetRequestToken("oob");
+
+            String uri = flickr.OAuthCalculateAuthorizationUrl(RequestToken.Token, AuthLevel.Write);
+            System.Diagnostics.Process.Start(uri);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String verify = txtVerifier.Text;
+            if (String.IsNullOrEmpty(verify))
+            {
+                MessageBox.Show("認証コードを入力してください");
+                return;
+            }
+
+            Flickr flickr = new Flickr(APIKey, Secret);
+            try
+            {
+                var Accsesstoken = flickr.OAuthGetAccessToken(RequestToken, verify);
+                txtResult.AppendText("ok");
+                txtResult.AppendText(Accsesstoken.FullName);
+
+            }
+            catch(FlickrApiException ex)
+            {
+                txtResult.AppendText(ex.ToString());
+            }
+        }
+
     }
 }
